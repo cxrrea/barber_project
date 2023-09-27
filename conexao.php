42ca6dd3f04f6ba1092396947642fd37f0078628
cxrrea <142359782+cxrrea@users.noreply.github.com>
2023-09-27 10:47:30 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -1,22 +1,9 @@
-<?php 
-session_start();
-    
-include_once("conexao.php");
+<?php
 
-      $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING) ;
-      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-      $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_STRING);
-      $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
+$dbHost = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "don_navalhon";
 
-      $result_usuario = "INSERT INTO dn_clientes (Nome_Completo, Email, Telefone, Senha) VALUES('$nome', '$email', '$telefone', '$senha')";
+$conn  = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
 
-     $resultado_usuario = mysqli_query($conn, $result_usuario);
-
-     if(mysqli_insert_id($conn)) {
-        $_SESSION['msg'] = "";
-        header("Location: index.php");
-     } else {
-        header("Location: index.php");
-     }
-     
-    
